626d228494983d931af98ce4b4ccb9370d07bed1
sandesh2526 <sandesh.parbhankar@gmail.com>
2020-10-23 20:19:25 +0530

    :)

--- search.js
+++ search.js
@@ -1,23 +1,26 @@
+row1 = document.getElementsByClassName("row-1");
+window.alert("HELLO");
+console.log(row1[0].innerText);
+
 function myFun()
 {
     var txt,products,val;
     input = document.getElementById("input").value.toUpperCase();
-
-    products = document.getElementsByClassName("card-title");
-    console.log(input);
-    for (let i = 0; i < products.length; i++) 
+    products = document.getElementsByClassName("card");
+    row1 = document.getElementsByClassName("row-1");
+    for (let count = 0; count < products.length; count++) 
     {
-        val = products[i].innerText;
+        val = products[count].innerText;
         txt = val.toUpperCase();
-        
         if(txt.indexOf(input) > -1)
         {
             console.log(val);
-            products[i].style.display = "";
+            products[count].style.display = "flex";
+            products[count].align
         }
         else
         {
-            products[i].style.display = "none";
+            products[count].style.display = "none";
         }
     }
 }
